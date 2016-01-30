9df51ccd63cc75fe2a9f21d6ff2c0fe9ff8732f9
Simon Gawlik <sgawlik@gmail.com>
2016-01-29 22:58:01 -0500

    ex9

--- ex9.c
+++ ex9.c
@@ -46,6 +46,15 @@ int main(int argc, char *argv[])
     printf("another each: %c %c %c %c\n",
             another[0], another[1],
             another[2], another[3]);
-            
+    
+    numbers[0] = name[0];
+    numbers[1] = name[1];
+    numbers[2] = name[2];
+    numbers[3] = name[3];
+    
+    printf("numbers: %d %d %d %d\n",
+            numbers[0], numbers[1],
+            numbers[2], numbers[3]);
+    
     return 0;
 }
