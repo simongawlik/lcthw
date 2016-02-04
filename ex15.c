bf33f8c95c2d4c589a8a019a244008535acb0208
Simon Gawlik <sgawlik@gmail.com>
2016-02-03 23:20:10 -0500

    ex15 update

--- ex15.c
+++ ex15.c
@@ -51,3 +51,19 @@ int main(int argc, char *argv[])
     
     return 0;
 }
+
+
+/*
+type *ptr
+"a pointer of type named ptr"
+*ptr
+"the value of whatever ptr is pointed at"
+*(ptr + i)
+"the value of (whatever ptr is pointed at plus i)"
+&thing
+"the address of thing"
+type *ptr = &thing
+"a pointer of type named ptr set to the address of thing"
+ptr++
+"increment where ptr points"
+*/
