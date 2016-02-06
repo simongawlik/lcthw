d0945eae71101cfeadda880c03b40a945d46ef43
Simon Gawlik <sgawlik@gmail.com>
2016-02-06 00:08:23 -0500

    update to ex16

--- ex16.c
+++ ex16.c
@@ -12,7 +12,7 @@ struct Person {
 
 struct Person *Person_create(char *name, int age, int height, int weight)
 {
-    struct Person *who = malloc(sizeof(struct Person))
+    struct Person *who = malloc(sizeof(struct Person));
     assert(who != NULL);
     
     who->name = strdup(name);
@@ -34,7 +34,7 @@ void Person_destroy(struct Person *who)
 void Person_print(struct Person *who)
 {
     printf("Name: %s\n", who->name);
-    prinft("\tAge: %d\n", who->age);
+    printf("\tAge: %d\n", who->age);
     printf("\tHeight: %d\n", who->height);
     printf("\tWeight: %d\n", who->weight);
 }
@@ -47,5 +47,27 @@ int main(int argc, char *argv[])
     
     struct Person *frank = Person_create(
             "Frank Blank", 20, 72, 180);
-
+            
+    // print them out and where they are in memory
+    printf("Joe is at memory location %p:\n", joe);
+    Person_print(joe);
+    
+    printf("Frank is at memory location %p:\n", frank);
+    Person_print(frank);
+    
+    // make everyone age 20 years and print them again
+    joe->age += 20;
+    joe->height -= 2;
+    joe->weight += 40;
+    Person_print(joe);
+    
+    frank->age += 20;
+    frank->weight += 20;
+    Person_print(frank);
+    
+    // destroy them both so we clean up
+    Person_destroy(joe);
+    Person_destroy(frank);
+    
+    return 0;
 }
